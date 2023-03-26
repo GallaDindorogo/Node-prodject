cd4c079cadf95d32ed51e58b807ae50db5cc62c7
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-03-26 16:42:48 +0200

    add hw-01

--- contacts.js
+++ contacts.js
@@ -1,16 +1,80 @@
 const path = require("path");
 const fs = require("fs/promises");
-import { uid } from "uid";
+const { uid } = require("uid");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 const listContacts = async () => {
   try {
-    const text = await fs.readFile(contactsPath, "utf-8");
-    console.log(text);
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
   } catch (error) {
     console.log(error.message);
   }
+};
 
-  // ...твой код
+const getContactById = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === id);
+    return result || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const removeContact = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const addContact = async ({ name, email, phone }) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: uid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 4));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const updateById = async (id, data) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return null;
+    }
+    contacts[index] = { id, ...data };
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index];
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  updateById,
+  removeContact,
 };
